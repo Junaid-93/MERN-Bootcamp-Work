6bcc73e829b9b1e9a1112a5bdb7bba151760f1ec
Junaid <engr.hja@gmail.com>
2023-08-30 01:48:37 +0500

    Edited some files

--- React Practice/vite-project/src/App.jsx
+++ React Practice/vite-project/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import CoursesList from './ListOfCourses'
@@ -10,38 +11,41 @@ import SearchParams from './prac'
 import PracArray from './prac'
 
 function App() {
-  //  const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0)
+  
+
 
   return (
-    <div>
-      {/* <div>
-         <a href="https://vitejs.dev" target="_blank">
-           <img src="/vite.svg" className="logo" alt="Vite logo" />
-         </a>
-         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-         </a>
-       </div>
-       <h1>Vite + React</h1>
-       <div className="card">
-         <button onClick={() => setCount((count) => count + 1)}>
-           count is {count}
-         </button>
-         <p>
-           Edit <code>src/App.jsx</code> and save to test HMR
-         </p>
-       </div>
-       <p className="read-the-docs">
-         Click on the Vite and React logos to learn more
-       </p> */}
-       {/* <CoursesList /> */}
-       {/* <Count /> */}
-        {/* <Count2 />  */}
-       <ToDoList />
-        {/* <PracArray /> */}
-        {/* <Abc /> */}
-        
-    </div>
+  <div>
+// {/*     
+//       <div>
+//          <a href="https://vitejs.dev" target="_blank">
+//            <img src="/vite.svg" className="logo" alt="Vite logo" />
+//          </a>
+//          <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//          </a>
+//        </div>
+//        <h1>Vite + React</h1>
+//        <div className="card">
+//          <button onClick={() => setCount((count) => count + 1)}>
+//            count is {count}
+//          </button>
+//          <p>
+//            Edit <code>src/App.jsx</code> and save to test HMR
+//          </p>
+//        </div>
+//        <p className="read-the-docs">
+//          Click on the Vite and React logos to learn more
+//        </p>
+//        <CoursesList />
+//        <Count />
+//         <Count2 /> 
+      //  <ToDoList />
+//         <PracArray />
+//         <Abc />
+//        </div>  */}
+</div>    
   )
 }
 
